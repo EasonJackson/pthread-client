be52a875b0d80dc785cb3f2734807a11359e22fb
EasonJackson <pyhanson@gmail.com>
2018-05-21 13:05:30 -0400

    Redo client end

--- OnlineChatClient/main.c
+++ OnlineChatClient/main.c
@@ -21,16 +21,11 @@
 # define SERVER_ADDRESS "localhost"
 # define SERVER_PORT 8080
 
-void* read_from_server(char[], int*);
-void* write_to_server(char[], char[], int*);
-
 static const ssize_t BUFFER_SIZE = 1024;
 static char sendmessage[BUFFER_SIZE];
 static char recvmessage[BUFFER_SIZE];
 
 int main(int argc, const char * argv[]) {
-    
-    pthread_t read_thread, write_thread;
     
     int server_socket_fd = 0;
     struct sockaddr_in server_address;
@@ -75,64 +70,6 @@ int main(int argc, const char * argv[]) {
         fprintf(stdout, "%s", recvmessage);
     }
     
-//    // Initialize threads
-//    if (0 > (pthread_create(&read_thread, NULL, read_from_server(recvmessage,
-//                                                                 &server_socket_fd), NULL))) {
-//        fprintf(stderr, "Cannot create new thread.");
-//    }
-//
-//    if (0 > (pthread_create(&write_thread, NULL, write_to_server(sendmessage,
-//                                                                 recvmessage,
-//                                                                 &server_socket_fd), NULL))) {
-//        fprintf(stderr, "Cannot create new thread.");
-//    }
-//
-//    pthread_join(read_thread, NULL);
-//    pthread_join(write_thread, NULL);
     close(server_socket_fd);
     return 0;
-}
-
-void* read_from_server(char recvmessage[BUFFER_SIZE],
-                       int* server_socket_fd_p) {
-    fprintf(stdout, "Created a read thread...\n");
-    
-    while(1) {
-        bzero(recvmessage, BUFFER_SIZE);
-        ssize_t num_bytes = read(*server_socket_fd_p, recvmessage, BUFFER_SIZE);
-
-        if (num_bytes == 0) {
-            fprintf(stderr, "Client cannot read from server\n");
-        } else {
-            fprintf(stdout, "Message from server %d: %s", *server_socket_fd_p, recvmessage);
-        }
-    }
-    return NULL;
-}
-
-void* write_to_server(char sendmessage[BUFFER_SIZE],
-                      char recvmessage[BUFFER_SIZE],
-                      int* server_socket_fd_p) {
-    fprintf(stdout, "Created a write thread...\n");
-    
-    while(1) {
-        bzero(sendmessage, BUFFER_SIZE);
-        bzero(recvmessage, BUFFER_SIZE);
-        
-        char* res = fgets(sendmessage, BUFFER_SIZE, stdin);
-        fprintf(stdout, "Get message from stdin\n");
-
-        if (res != NULL) {
-            if (0 > write(*server_socket_fd_p, sendmessage, BUFFER_SIZE)) {
-                fprintf(stderr, "Client cannot send message to server\n");
-            } else {
-                fprintf(stdout, "Send a message to server\n");
-            }
-        }
-        
-        read(*server_socket_fd_p, recvmessage, BUFFER_SIZE);
-        fprintf(stdout, "%s", recvmessage);
-    }
-    
-    return NULL;
 }
